b373733f221acc576314c06af527d57d8787cfe7
Kyle KIM <k2wackie@gmail.com>
2022-04-15 16:21:20 +0900

    Using useCallback & 함수형 업데이트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import OptimizeTest from "./OptimizeTest";
 
 function App() {
   const [data, setData] = useState([]);
@@ -32,7 +31,7 @@ function App() {
     };
   }, []);
 
-  const onCreate = (author, content, emotion) => {
+  const onCreate = useCallback((author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
       author,
@@ -42,8 +41,8 @@ function App() {
       id: dataId.current,
     };
     dataId.current += 1;
-    setData([newItem, ...data]);
-  };
+    setData((data) => [newItem, ...data]); //함수형 업데이트
+  }, []);
 
   const onRemove = (targetId) => {
     console.log(`${targetId}가 삭제되었습니다.`);
@@ -72,7 +71,6 @@ function App() {
 
   return (
     <div className="App">
-      <OptimizeTest />
       <DiaryEditor onCreate={onCreate} />
       <div>전체 일기: {data.length}</div>
       <div>기분 좋은 일기 개수 : {goodCount}</div>
